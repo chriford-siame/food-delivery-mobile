bf44b564e141ebef8c56c08e2013d080a6ee2275
chriford-siame <chrifordsiame@gmail.com>
2023-09-03 23:56:00 +0200

    cleanup the item details screen

--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -15,38 +15,25 @@ const DetailsScreen = ({ navigation, route }) => {
                 </View>
             </View>
             <View>
-                <Image source={food.image} style={styles.foodImage} />
+                <Image source={food.image} style={styles.item_image} />
             </View>
-            <View style={styles.foodDetail}>
-                <View>
-                    <View style={{
-                        justifyContent: 'space-between',
-                        flexDirection: 'row',
-                    }}>
-                        <Text style={{ fontSize: 25, color: COLORS.white }}>{food.name}</Text>
-                        <View style={{
-                            height: 50,
-                            width: 50,
-                            borderRadius: 100,
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            backgroundColor: COLORS.white,
-                        }}>
+            <View style={styles.item}>
+                <View style={styles.item_container}>
+                    <View style={{justifyContent: 'space-between',flexDirection: 'row'}}>
+                        <Text style={styles.item_container_name}>{food.name}</Text>
+                        <View style={styles.item_container_favorite_icon}>
                             <Icon name='favorite' size={20} color={COLORS.orange} />
                         </View>
                     </View>
                 </View>
-                <View style={{
-                    top: 20,
-                    paddingBottom: 10,
-                }}>
-                    <Text style={{fontSize: 18, color: COLORS.white}}>
+                <View style={styles.item_description_container}>
+                    <Text style={styles.item_container_description}>
                         {food.description}
                     </Text>
                 </View>
                 <TouchableOpacity activeOpacity={0.5} onPress={() => navigation.navigate('CartScreen', food)}>
                     <View style={styles.btn}>
-                        <Text style={{fontSize: 20, color: COLORS.orange, fontWeight: 'bold'}}>Add To Cart</Text>
+                        <Text style={styles.item_cart_button}>Add To Cart</Text>
                     </View>
                 </TouchableOpacity>
             </View>
@@ -66,6 +53,7 @@ const styles = StyleSheet.create({
     },
     header_container: { flexDirection: 'row' },
     header_container_title: {color: COLORS.dark, fontSize: 20},
+    
     btn: {
         justifyContent: 'center', 
         alignItems: 'center',
@@ -74,15 +62,8 @@ const styles = StyleSheet.create({
         borderRadius: 20,
         padding: 10,
     },
-    foodImage: {
-        width: "55%",
-        height: 220,
-        borderRadius: 200,
-        alignSelf: 'center',
-        borderColor: COLORS.primary,
-        borderWidth: 2,
-    },
-    foodDetail: {
+
+    item: {
         top: 20,
         bottom: 0,
         backgroundColor: COLORS.orange,
@@ -90,7 +71,31 @@ const styles = StyleSheet.create({
         borderTopLeftRadius: 40,
         padding: 20,
         height: "100%"
-    }
+    },
+    item_image: {
+        width: "55%",
+        height: 220,
+        borderRadius: 200,
+        alignSelf: 'center',
+        borderColor: COLORS.primary,
+        borderWidth: 2,
+    },
+    item_container: {},
+    item_container_name: { fontSize: 25, color: COLORS.white },
+    item_description_container: {
+        top: 20,
+        paddingBottom: 10,
+    },
+    item_container_description: {fontSize: 18, color: COLORS.white},
+    item_container_favorite_icon: {
+        height: 50,
+        width: 50,
+        borderRadius: 100,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: COLORS.white,
+    },
+    item_cart_button: {fontSize: 20, color: COLORS.orange, fontWeight: 'bold'},
 })
 
 export default DetailsScreen;
